d82289090642b81e2023b4517dd5672e75a78d51
Anna Rachel Valdez <annarachel.valdez@gmail.com>
2020-10-18 01:59:37 -0700

    Still need to implement antd style

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,77 +1,194 @@
-import React from 'react'
-import { Formik } from 'formik'
-import { useForm } from 'antd/lib/form/Form'
-import * as Yup from 'yup'
+import React, {useState}from 'react'
 
 
-// const validationSchema = Yup.object({
-//     name:
-// })
+import {Input, Button, Form as FormA  } from 'antd'
+import "antd/dist/antd.css";
+import * as yup from 'yup'
+import {
+    Formik,
+    Form,
+    Field,
+    ErrorMessage,
+    FieldArray,
+    FastField,
+    useFormik,
+    withFormik,
+    FormikErrors,
+     FormikProps
+  } from 'formik'
+import { render } from 'react-dom';
+
+//   import { Form as FormA, SubmitButton, ResetButton /* ... */ } from 'formik-antd'
+
+const layout = {
+    labelCol: {
+      span: 8,
+    },
+    wrapperCol: {
+      span: 16,
+    },
+  };
+  const tailLayout = {
+    wrapperCol: {
+      offset: 8,
+      span: 16,
+    },
+  };
+
+
+  const initialValues = {
+    firstName: '',
+    email: '',
+    username: ''
+  }
+
+  const validationSchema = yup.object({
+    firstName: yup.string()
+        .required('First name is required'),
+    email: yup.string()
+        .email('invalid email format')
+        .required('Email required'),
+    password: yup
+        .string()
+        .required("Please enter your password")
+        .matches(
+          /^.*(?=.{8,})((?=.*[!@#$%^&*()\-_=+{};:,<.>]){1})(?=.*\d)((?=.*[a-z]){1})((?=.*[A-Z]){1}).*$/,
+          "Password must contain at least 8 characters, one uppercase, one number and one special case character"
+        ),
+    confirmPassword: yup
+        .string()
+        .required("Please confirm your password")
+        .when("password", {
+          is: password => (password && password.length > 0 ? true : false),
+          then: yup.string().oneOf([yup.ref("password")], "Password doesn't match")
+        })
+  })
+
+  //need to connect this to backend
+  const onSubmit = values => {
+      console.log('Form data', values)
+  }
+
+  
+
 function SignUp () {
-    const initialValues = {
-        firstName: '',
-        lastName: '',
-        email: '',
-        username: ''
-      }
-
-      const validationSchema = Yup.object({
-        firstName: Yup.string()
-            .required('First name is required'),
-        lastName: Yup.string()
-            .required("Last name is required"),
-        email: Yup.string()
-            .email('invalid email format')
-            .required('Email required')
-      })
-
-      //need to connect this to backend
-      const onSubmit = values => {
-          console.log('Form data', values)
-      }
-    
     return(
         <Formik
             initialValues={initialValues}
             validationSchema={validationSchema}
             onSubmit={onSubmit}
-        >
-        {
-            formik => {
-                return (
-                    <Form>
-                        <FormikControl
-                            control='input'
-                            type='text'
-                            label='firstName'
-                            name='firstName'
-                        />
+            > 
+ { props=>(
+          <Form> 
+       
+          <FormA.Item>
+                <Input name="email" 
+                placeholder="Email"
+                value ={props.values.email}
+                onChange={props.handleChange}
+                onBlue={props.handleBlur}
+                />
+              </FormA.Item>
 
-                         <FormikControl
-                            control='input'
-                            type='text'
-                            label='lastName'
-                            name='lastName'
-                        />
-                         <FormikControl
-                            control='input'
-                            type='email'
-                            label='Email'
-                            name='email'
-                        />
 
-                        <button type='submit' disabled={!formik.isValid}>
-                          Submit
-                        </button>
-                    </Form>
-                )
-            }
-        }
+            <div className='form-control'>
+              <label htmlFor='name'>First Name</label>
+              <Field 
+                type='text'
+                id='firstName'
+                name='firstName'
+                />
+              <ErrorMessage name='firstName' />
+            </div>
 
-        </Formik>
+            <div className='form-control'>
+              <label htmlFor='email'>Email</label>
+              <Field 
+                type='email'
+                id='email'
+                name='email'
+                />
+              <ErrorMessage name='email' />
+            </div>
 
-    )
+
+            <div className='form-control'>
+              <label htmlFor='password'>Password</label>
+              <Field 
+                type='password'
+                id='password'
+                name='password'
+                />
+              <ErrorMessage name='password' />
+            </div>
+            
+            <div className='form-control'>
+              <label htmlFor='text'>Confirm Password</label>
+              <Field 
+                type='password'
+                id='confirmPassword'
+                name='confirmPassword'
+                />
+              <ErrorMessage name='confirmPassword' />
+            </div>
+
+            <Button type='primary' htmlType='submit' >Submit</Button>
+
+
+
+
+          </Form>
+       ) }
+      
+    </Formik>
    
-}
+  )
+
+   }
 
 export default SignUp
+
+
+{/* // </Formik>/Formik>return( */}
+//     <Formik
+//         initialValues={initialValues}
+//         validationSchema={validationSchema}
+//         onSubmit={onSubmit}
+//     >
+//     {
+//         formik => {
+//             return (
+//                 <Form>
+//                     <FormikControl
+//                         control='input'
+//                         type='text'
+//                         label='First Name'
+//                         name='firstName'
+//                     />
+//                      <FormikControl
+//                         control='input'
+//                         type='email'
+//                         label='Email'
+//                         name='email'
+//                     />
+//                     <FormikControl
+//                         control='input'
+//                         type='password'
+//                         label='Password'
+//                         name='password'
+//                     />
+//                     <FormikControl
+//                         control='input'
+//                         type='password'
+//                         label='Confirm Password'
+//                         name='confirmPassword'
+//                     />
+//                     <button type='submit' disabled={!formik.isValid}>
+//                       Submit
+//                     </button>
+//                 </Form>
+//             )
+//         }
+//     }
+
+//     </Formik>
