9b3f01e36b3a2f6d664d068104bb8c7b3092cb03
Anna Rachel Valdez <annarachel.valdez@gmail.com>
2020-10-20 21:50:56 -0700

    upload files page still on the process

--- src/pages/UploadFiles.js
+++ src/pages/UploadFiles.js
@@ -1,56 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'antd/dist/antd.css';
-
-import { Upload, Button } from 'antd';
+import React,  { useState, useEffect }from 'react'
+import { Upload, message, Button } from 'antd';
 import { UploadOutlined } from '@ant-design/icons';
+import useFileHandlers from '../components/useFileHandlers'
 
-class UploadFiles extends React.Component {
-  state = {
-    fileList: [
-      {
-        uid: '-1',
-        name: 'xxx.png',
-        status: 'done',
-        url: 'http://www.baidu.com/xxx.png',
-      },
-    ],
-  };
+const Input = (props) => (
+    <input
+      type="file"
+      accept="image/*"
+      name="img-loader-input"
+      multiple
+      {...props}
+    />
+  )
+function UploadFiles() {
 
-  handleChange = info => {
-    let fileList = [...info.fileList];
-
-    // 1. Limit the number of uploaded files
-    // Only to show two recent uploaded files, and old ones will be replaced by the new
-    fileList = fileList.slice(-2);
-
-    // 2. Read from response and show file link
-    fileList = fileList.map(file => {
-      if (file.response) {
-        // Component will show file.url as link
-        file.url = file.response.url;
-      }
-      return file;
-    });
-
-    this.setState({ fileList });
-  };
-
-  render() {
-    const props = {
-      action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
-      onChange: this.handleChange,
-      multiple: true,
-    };
-    return (
-      <Upload {...props} fileList={this.state.fileList}>
-        <Button icon={<UploadOutlined />}>Upload</Button>
-      </Upload>
-    );
-  }
+ 
 }
-
-ReactDOM.render(<UploadFiles />, document.getElementById('container'));
-
 
 export default UploadFiles
